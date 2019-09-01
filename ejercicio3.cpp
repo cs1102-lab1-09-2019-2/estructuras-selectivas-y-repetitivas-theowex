e755f2a7ceca89868c417a2cdcaca0645e0ed3d1
Owen Atamari <32944160+theowex@users.noreply.github.com>
2019-09-01 17:43:58 -0500

    Update ejercicio3.cpp

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,6 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
+using namespace std;
 
 int main() {
-
-    return 0;
+    int n, temp;
+    vector<int> lista;
+    cout<<"n: ";
+    cin>>n;
+    for(int i = 0; i < n; i++) {
+        cin>>temp;
+        lista.push_back(temp);
+    }
+    sort(lista.begin(), lista.end());
+    cout<<"El valor maximo es: "<<lista[lista.size()-1];
 }
