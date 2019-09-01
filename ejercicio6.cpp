97cd850bf53afd00c5c9297961f5ecbf383d7d7b
Owen Atamari <32944160+theowex@users.noreply.github.com>
2019-09-01 17:58:03 -0500

    Update ejercicio6.cpp

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -1,6 +1,18 @@
+#include <iostream>
 
+using namespace std;
 
 int main() {
-
-    return 0;
+    int n;
+    cout<<"N: ";
+    cin>>n;
+    for(int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i%2 == 0 && j%2 == 0 || i%2 != 0 && j%2 != 0)
+                cout<<'#';
+            else
+                cout<<'@';
+        }
+        cout << "\n";
+    }
 }
