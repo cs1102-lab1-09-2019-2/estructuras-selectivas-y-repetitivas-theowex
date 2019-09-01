56d9fb34f37f73c64bbd0a9aeaec42b799b7df20
Owen Atamari <32944160+theowex@users.noreply.github.com>
2019-09-01 17:21:01 -0500

    Update ejercicio2.cpp

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,6 +1,17 @@
+#include <iostream>
 
+using namespace std;
 
 int main() {
-
-    return 0;
+    int n;
+    cin>>n;
+    for(int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j || i == 0 || j == 0 || j == n - 1 || i == n - 1)
+                cout<<'*';
+            else
+                cout<<' ';
+        }
+        cout << "\n";
+    }
 }
