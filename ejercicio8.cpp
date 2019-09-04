6efc577fca6ddf98909240977acb81c8bff98b2b
Owen Atamari <32944160+theowex@users.noreply.github.com>
2019-09-04 17:52:32 -0500

    Update ejercicio8.cpp

--- ejercicio8.cpp
+++ ejercicio8.cpp
@@ -1,7 +1,14 @@
+#include <iostream>
 
+using namespace std;
 
 int main() {
-
-    return 0;
+    int a, b, c, d;
+    cin>>a>>b>>c>>d;
+    if(max(3*a/10, a - (a*c)/250) > max(3*b/10, b - (b*d)/250))
+        cout<<"Alessia";
+    else if(max(3*a/10, a - (a*c)/250) < max(3*b/10, b - (b*d)/250))
+        cout<<"Jasmin";
+    else
+        cout<<"Empate";
 }
-
