590f548bc2b531851de106dc49a689c8bf4cc6e0
Owen Atamari <32944160+theowex@users.noreply.github.com>
2019-09-04 15:56:05 -0500

    Update ejercicio5.cpp

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -1,6 +1,18 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
 
+using namespace std;
 
 int main() {
-
-    return 0;
+    float cant, temp, media, suma = 0, sumax = 0;
+    cout<<"N: ";
+    cin>>cant;
+    for(int i = 0; i < cant; i++){
+        cout<<'x'<<i+1<<": ";
+        cin>>temp;
+        suma += temp;
+        sumax += pow(temp, 2);
+    }
+    cout<<"sd: "<<fixed<<setprecision(2)<<sqrt(sumax/cant - pow(suma/cant,2));
 }
