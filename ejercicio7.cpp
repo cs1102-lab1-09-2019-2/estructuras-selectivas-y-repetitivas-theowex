a754a3eef0c3c39098a68c85b1fa78f167f4852e
Owen Atamari <32944160+theowex@users.noreply.github.com>
2019-09-04 20:51:40 -0500

    Update ejercicio7.cpp

--- ejercicio7.cpp
+++ ejercicio7.cpp
@@ -1,7 +1,24 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
 
 int main() {
-
-    return 0;
+    string num;
+    cin>>num;
+    cout<<"SOAT:";
+    if(int(num[4])%2 == 0)
+        cout<<"Vigente";
+    else
+        cout<<"Vencido";
+    cout<<endl<<"CITV:";
+    if(int(num[5])%2 == 0)
+        cout<<"Vigente";
+    else
+        cout<<"Vencido";
+    cout<<endl<<"AUTORIZADO:";
+    if(int(num[6] + num[7])%3 == 0)
+        cout<<"Si";
+    else
+        cout<<"No";
 }
-
